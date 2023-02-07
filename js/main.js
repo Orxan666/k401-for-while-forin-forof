37a61cb0f2970eaee63322878b29910183b1349c
orxan666 <orxanallahyarov03@gmail.com>
2023-02-07 12:57:55 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+'use strict';
+// let a=0
+// let myArr=[1,3,5,7]
+
+// while(a<myArr.length){
+//     console.log(myArr[a]);
+//     a++
+
+// }
+
+// let counter = 1
+// while (counter <= 5) {
+//     counter++
+//     console.log(`sizin reqeminiz ${counter}`);
+
+// }
+
+// let a=1
+
+// do {
+//     console.log("Sizin daxil etdiyiniz reqem ardicilligi "+a);
+//     a++
+// } while (a<4);
+
+
+// let a = 1
+
+// while (a < 20) {
+//     console.log(a);
+//     if (a == 15) {
+//         break
+//     }
+//     a++
+// }
+
+let i = 0
+let arr1 = ["orxan", "eli", "huseyn", "susan"]
+
+// for(let value of arr1 ){
+//     console.log(value);
+// }
+// for (const value in arr1) {
+// console.log(arr1[value]);
+// }
+const developer={
+    ad:["Orxan","Eli","Vuqar"],
+    salary:"100"
+}
+for (const key in developer) {
+    console.log(developer[key]);
+} 
+// for of yalniz arrayda istifade olunur ve value qaytarir 
+
+
+// while (i < arr1.length) {
+
+//     console.log(arr1[i]);
+
+//     i++
+
+// }
+
+// for (let i = 0; i < arr1.length; i += 1) {
+//     // console.log(arr1[i]);
+//     if (arr1[i][0] === 's') {
+//         console.log(arr1[i]);
+//     }
+// }
